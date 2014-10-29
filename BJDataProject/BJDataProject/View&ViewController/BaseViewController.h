ba65755239f72847e06f6a0346371ab57cdbc663
yanxiaoliang-bjhl <ouyaliang@163.com>
2014-10-29 16:23:17 +0800

    listViewController

---
+++ BJDataProject/BJDataProject/View&ViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  BJDataProject
+//
+//  Created by Randy on 14/10/29.
+//  Copyright (c) 2014年 Randy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
